af73e8d3a534c241328841e63ab64ec2bac19dde
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-04-28 18:09:24 +0200

    problema di calcolare il percorso per tutte le destinazioni interne all'area dell'agente e non solo la prima

--- Simulation/CBS/cbs.py
+++ Simulation/CBS/cbs.py
@@ -300,8 +300,7 @@ class CBS(object):
             self.env.constraint_dict = P.constraint_dict
             conflict_dict = self.env.get_first_conflict(P.solution)
             if not conflict_dict:
-                print("Low level CBS - Solution found")
-
+                #print("Low level CBS - Solution found")
                 return self.generate_plan(P.solution)
 
             constraint_dict = self.env.create_constraints_from_conflict(conflict_dict)
