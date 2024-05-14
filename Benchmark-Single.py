09659b34e947552a1b05d52717088671e61987d2
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-05-14 17:18:48 +0200

    mappa con corridoi grandi

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     array_Astar_exp_sum_max_per_timestep = []
 
 
-    file_name = 'Comparisons/partition/1.txt'
+    file_name = 'Comparisons/corridoi2/a8.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
