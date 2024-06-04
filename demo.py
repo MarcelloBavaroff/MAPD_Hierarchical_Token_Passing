b8f6449cacd3923cb013e1bbeb2bd1aa264c3188
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-06-04 22:20:02 +0200

    p7 fixed

--- demo.py
+++ demo.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('-a_star_max_iter', help='Maximum number of states explored by the low-level algorithm',
                         default=1000, type=int)
     parser.add_argument('-slow_factor', help='Slow factor of visualization', default=10, type=int) #default=1
-    parser.add_argument('-not_rand', help='Use if input has fixed tasks and delays', action='store_true', default=False)
+    parser.add_argument('-not_rand', help='Use if input has fixed tasks and delays', action='store_true', default=True)
 
     args = parser.parse_args()
 
