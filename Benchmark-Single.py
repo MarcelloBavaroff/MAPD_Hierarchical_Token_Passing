468239c598082c29eeb355585c6e9f349eea190f
Marcello <marcello.bavaro@mail.polimi.it>
2024-06-09 00:36:39 +0200

    hp5

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     array_maxAstar = []
     array_parallel_rounds = []
 
-    file_name = 'Comparisons/Stern/13hp3.txt'
+    file_name = 'Comparisons/Stern/15hp5.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
