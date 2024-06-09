887bdd132664651fee670204dfeb30989bb79fcb
Marcello <marcello.bavaro@mail.polimi.it>
2024-06-09 17:53:40 +0200

    hp5

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -15,7 +15,7 @@ def parameters(seed):
     #random_seed = seed
     parser = argparse.ArgumentParser()
     parser.add_argument('-a_star_max_iter', help='Maximum number of states explored by the low-level algorithm',
-                        default=500, type=int)
+                        default=1000, type=int)
     parser.add_argument('-slow_factor', help='Slow factor of visualization', default=1, type=int)  # default=1
     parser.add_argument('-not_rand', help='Use if input has fixed tasks and delays', action='store_true', default=False)
     args = parser.parse_args()
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     array_maxAstar = []
     array_parallel_rounds = []
 
-    file_name = 'Comparisons/Stern/11hp5.txt'
+    file_name = 'Comparisons/Stern/14hp5.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
