fd55227c5e7650de77a7e617b579e491fe68e77e
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-06-19 15:08:24 +0200

    mappa h11p

--- Utils/create_map.py
+++ Utils/create_map.py
@@ -99,17 +99,17 @@ def create_frontiers_horizontal():
     # Inizializza la lista degli ostacoli
     frontiers = []
 
-    # Itera attraverso le righe e le colonne specificate
-    # k = 1
-    # for j in range(26, 87, 12):
-    #     for i in range(0, 121, 12):
-    #         # 31, 1, k+1, 30, 1, k
-    #         f = [i, j+1, k+1, i, j, k]
-    #         # 30, 2, k, 31, 2, k+1
-    #         ff = [i+1, j, k, i+1, j+1, k+1]
-    #         frontiers.append(f)
-    #         frontiers.append(ff)
-    #     k += 1
+    #Itera attraverso le righe e le colonne specificate
+    k = 5
+    for j in range(62, 95, 8):
+        for i in range(0, 121, 12):
+            # 31, 1, k+1, 30, 1, k
+            f = [i, j+1, k+1, i, j, k]
+            # 30, 2, k, 31, 2, k+1
+            ff = [i+1, j, k, i+1, j+1, k+1]
+            frontiers.append(f)
+            frontiers.append(ff)
+        k += 1
     #k = 0
     # for j in range(8, 55, 9):
     #     for i in range(4, 25, 2):
@@ -121,15 +121,15 @@ def create_frontiers_horizontal():
     #         frontiers.append(ff)
     #     k += 1
 
-    k=7
-    j=98
-    for i in range(0, 121, 2):
-        # 31, 1, k+1, 30, 1, k
-        f = [i, j + 1, k + 1, i, j, k]
-        # 30, 2, k, 31, 2, k+1
-        ff = [i + 1, j, k, i + 1, j + 1, k + 1]
-        frontiers.append(f)
-        frontiers.append(ff)
+    # k=7
+    # j=98
+    # for i in range(0, 121, 2):
+    #     # 31, 1, k+1, 30, 1, k
+    #     f = [i, j + 1, k + 1, i, j, k]
+    #     # 30, 2, k, 31, 2, k+1
+    #     ff = [i + 1, j, k, i + 1, j + 1, k + 1]
+    #     frontiers.append(f)
+    #     frontiers.append(ff)
 
     for f in frontiers:
         print("-    ", f)
