09e0c5e013b994d2420b951aadb3f116bac5620e
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-06-21 10:21:57 +0200

    mappa h11p pickup laterali

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     array_maxAstar = []
     array_parallel_rounds = []
 
-    file_name = 'Comparisons/Stern/21hp11+2a500.txt'
+    file_name = 'Comparisons/Stern/21hp11a500.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
