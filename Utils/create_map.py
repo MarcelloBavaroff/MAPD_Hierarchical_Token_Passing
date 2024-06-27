c60962dd7230abf397f616082bbfa0ae854e6f7b
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-06-27 16:36:24 +0200

    mappa 170x84 h7p

--- Utils/create_map.py
+++ Utils/create_map.py
@@ -111,7 +111,7 @@ def create_frontiers_horizontal():
     #     k += 1
 
     k=0
-    rows = [7, 15, 23, 35, 47, 59, 67, 75]
+    rows = [11, 23, 35, 47, 59, 71]
     for j in rows:
         for i in range(144, 165, 2):
             # 31, 1, k+1, 30, 1, k
