0d6772ce81f8d3ba4977fce5dcb1d7c9463ddc11
MarcelloBavaroff <61752474+MarcelloBavaroff@users.noreply.github.com>
2024-06-28 15:24:31 +0200

    mappa 122x114 h21p con task sopra e sotto

--- Utils/create_map.py
+++ Utils/create_map.py
@@ -113,21 +113,22 @@ def create_frontiers_horizontal():
     k=0
     rows = [3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63, 67, 71, 75, 79]
     for j in rows:
-        for i in range(144, 165, 2):
-            # 31, 1, k+1, 30, 1, k
-            f = [i, j + 1, k + 1, i, j, k]
-            # 30, 2, k, 31, 2, k+1
-            ff = [i + 1, j, k, i + 1, j + 1, k + 1]
-            frontiers.append(f)
-            frontiers.append(ff)
-        for i in range(4, 26, 2):
-            # 31, 1, k+1, 30, 1, k
-            f = [i, j + 1, k + 1, i, j, k]
-            # 30, 2, k, 31, 2, k+1
-            ff = [i + 1, j, k, i + 1, j + 1, k + 1]
-            frontiers.append(f)
-            frontiers.append(ff)
-        for i in range(36, 134, 12):
+        j = j+15
+        # for i in range(144, 165, 2):
+        #     # 31, 1, k+1, 30, 1, k
+        #     f = [i, j + 1, k + 1, i, j, k]
+        #     # 30, 2, k, 31, 2, k+1
+        #     ff = [i + 1, j, k, i + 1, j + 1, k + 1]
+        #     frontiers.append(f)
+        #     frontiers.append(ff)
+        # for i in range(4, 26, 2):
+        #     # 31, 1, k+1, 30, 1, k
+        #     f = [i, j + 1, k + 1, i, j, k]
+        #     # 30, 2, k, 31, 2, k+1
+        #     ff = [i + 1, j, k, i + 1, j + 1, k + 1]
+        #     frontiers.append(f)
+        #     frontiers.append(ff)
+        for i in range(0, 121, 12):
             # 31, 1, k+1, 30, 1, k
             f = [i, j + 1, k + 1, i, j, k]
             # 30, 2, k, 31, 2, k+1
