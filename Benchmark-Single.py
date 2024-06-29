20e518f1919df74623f2d7d2edf6c9ec4b6f3c5a
Marcello <marcello.bavaro@mail.polimi.it>
2024-06-29 22:31:52 +0200

    hp17

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -44,7 +44,7 @@ def parameters(seed):
 
 
     tasks = gen_tasks(param['map']['pickup_locations'], param['map']['delivery_locations'],
-                                             1000, 0.2, seed)
+                                             1000, 5, seed)
     param['tasks'] = tasks
 
     # with open('Comparisons/seeds2.txt', 'a') as file:
@@ -122,7 +122,7 @@ if __name__ == '__main__':
     array_maxAstar = []
     array_parallel_rounds = []
 
-    file_name = 'Comparisons/Stern/13hp15a500.txt'
+    file_name = 'Comparisons/Stern/11hp17a500.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
