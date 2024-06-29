8eb9a363cafb31cec8707a5ed9d47e5bac9aa995
Marcello <marcello.bavaro@mail.polimi.it>
2024-06-29 19:59:59 +0200

    hp13 e 15

--- Print_Matrix.py
+++ Print_Matrix.py
@@ -10,6 +10,9 @@ class PrintMatrix(object):
         self.heatmap = np.zeros((dimensions[0], dimensions[1]))
         for path in all_paths.values():
             for c in path:
+                #non considero i NON-TE
+                if c['x'] == 0 or c['x'] == dimensions[0] - 1 or c['y'] == 0 or c['y'] == dimensions[1] - 1:
+                    continue
                 self.heatmap[c['x'], c['y']] += 1
 
 
