45fe9fba4deaad1c0427d3e6b8ca84daabac47e7
Marcello <marcello.bavaro@mail.polimi.it>
2024-07-20 23:19:24 +0200

    nuovi test

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -22,7 +22,7 @@ def parameters(seed):
     parser.add_argument('-not_rand', help='Use if input has fixed tasks and delays', action='store_true', default=False)
     args = parser.parse_args()
 
-    with open(os.path.join(RoothPath.get_root(), 'config.json'), 'r') as json_file:
+    with open(os.path.join(RoothPath.get_root(), 'config2.json'), 'r') as json_file:
         config = json.load(json_file)
     args.param = os.path.join(RoothPath.get_root(), os.path.join(config['input_path'], config['input_name']))
     args.output = os.path.join(RoothPath.get_root(), 'output.yaml')
@@ -46,7 +46,7 @@ def parameters(seed):
 
 
     tasks = gen_tasks(param['map']['pickup_locations'], param['map']['delivery_locations'],
-                                             1000, 5, seed)
+                                             1000, 0.2, seed)
     param['tasks'] = tasks
 
     # with open('Comparisons/seeds2.txt', 'a') as file:
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     array_actual_paths = []
 
 
-    file_name = 'Comparisons/Stern/28p9a2000.txt'
+    file_name = 'Comparisons/Stern/29p3a2000.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
