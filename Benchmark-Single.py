42dccf5d97ff5a51aa8e2dc561f6d700bea7ca57
Marcello <marcello.bavaro@mail.polimi.it>
2024-07-25 09:26:55 +0200

    nuovi test

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     array_actual_paths = []
 
 
-    file_name = 'Comparisons/Stern/28p7a2000.txt'
+    file_name = 'Comparisons/Stern/28p11a2000.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
