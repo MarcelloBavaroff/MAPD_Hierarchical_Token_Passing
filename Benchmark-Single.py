ad9bf52bfd1d5b79af5cd7fad0474c7a354def24
georgemanca <george.mancarella@gmail.com>
2024-07-26 19:36:14 +0200

    test giorgio

--- Benchmark-Single.py
+++ Benchmark-Single.py
@@ -27,7 +27,7 @@ def parameters(seed):
     #random_seed = seed
     parser = argparse.ArgumentParser()
     parser.add_argument('-a_star_max_iter', help='Maximum number of states explored by the low-level algorithm',
-                        default=2000, type=int)
+                        default=500, type=int)
     parser.add_argument('-slow_factor', help='Slow factor of visualization', default=1, type=int)  # default=1
     parser.add_argument('-not_rand', help='Use if input has fixed tasks and delays', action='store_true', default=False)
     args = parser.parse_args()
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     array_actual_paths = []
 
 
-    file_name = 'Comparisons/Stern/Giorgio/28p7a2000.txt'
+    file_name = 'Comparisons/Stern/Giorgio/11Gp11a500.txt'
 
     with open('Comparisons/seeds1.txt', 'r') as file:
         # inserisci ogni riga in una lista
